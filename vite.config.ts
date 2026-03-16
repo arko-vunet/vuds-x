da5be6b6eec222ce929330e442b4b380ddf3437a
arko-vunet <arko@vunetsystems.com>
2026-03-16 11:31:10 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
